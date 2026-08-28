6fad4adce66e99c1dd30cf7750f75a4d0879c455
Isiri HS <isirihs23cs@rnsit.ac.in>
2026-08-28 17:34:37 +0530

    Fix CORS for deployed frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -42,7 +42,8 @@ origins = [
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://recoverai-frontend-efxi.onrender.com"
 ]
 
 app.add_middleware(
